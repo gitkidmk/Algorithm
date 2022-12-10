fa51058b68a650373943bb57d6775b145ebf114c
강민경 <minkyeong_kang@tmax.co.kr>
2022-12-10 16:34:36 +0900

    [feat] java array of linkedlist
    - 멀티 쓰레드에서 synchronized 문제 발생하는 코드임

--- DFS&BFS/11724.java
+++ DFS&BFS/11724.java
@@ -3,14 +3,30 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Arrays;
 import java.util.LinkedList;
-import java.util.List;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
 class Main{
-    void dfs(int node){
 
+    LinkedList<Integer>[] graph;
+    static boolean[] visited;
+
+    void dfs(int node){
+        //주어진 node를 stack에 담기
+        Stack<Integer> stack = new Stack<>();
+        stack.push(node);
+
+        while (!stack.empty()){
+            int n = stack.pop();
+            if(visited[n] == false){
+                visited[n] = true;
+                for(int el : graph[n]){
+                    stack.push(el);
+                }
+            }
+        }
+        
     }
 
     void solution() throws IOException{        
@@ -19,13 +35,19 @@ class Main{
 
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
+        int answer = 0;
+        visited = new boolean[n+1];
 
-        //2차원 배열의 1차는 Array, 2차는 List
-        LinkedList<Integer>[] graph = new LinkedList[n+1];
-        boolean[] visited = new boolean[n+1];
+        //Array of LinkedList인 2차원 배열 생성 : warning 분석
+        graph = new LinkedList[n+1];
+
+        // Array of LinkedList 초기화
+        for(int i=1; i<n+1; i++){
+            graph[i] = new LinkedList<Integer>();
+        }
         
-        // List graph = new LinkedList<Integer>();
-        for(int i = 1; i < n+1; i++){
+        // graph에 연관관계 채워넣기
+        for(int i = 1; i < m+1; i++){
             StringTokenizer st2 = new StringTokenizer(br.readLine()," ");
 
             int o = Integer.parseInt(st2.nextToken());
@@ -35,12 +57,15 @@ class Main{
             graph[p].add(o);
         }
 
-        System.out.println(Arrays.toString(graph) + " & " + Arrays.toString(visited));
-
-
+        for(int i = 1; i < n+1; i++){
+            if(visited[i]==false){
+                dfs(i);
+                answer ++;
+            }
+        }
         
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        bw.write("TEST...!");
+        bw.write(Integer.toString(answer));
         bw.flush();
         bw.close();
     }
